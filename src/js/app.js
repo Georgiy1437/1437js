b5848f264086b98e0bd339979d8c1d07c6045e67
Georgiy1437 <g.g.shishkin@gmail.com>
2021-09-03 23:08:49 +0400

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
 const app = new PIXI.Application({ width: 800, height: 800 });
 document.body.appendChild(app.view);
 
-const graphics = new PIXI.Graphics();
+//const graphics = new PIXI.Graphics();
 
 // Rectangle
-graphics.beginFill(0xFF0000);
-graphics.drawRect(0, 0, 100, 100);
-app.stage.addChild(graphics);
+//graphics.beginFill(0xFF0000);
+//graphics.drawRect(0, 0, 100, 100);
+//app.stage.addChild(graphics);
 
+//graphics.x = app.screen.width / 2 - 50;
+//graphics.y = app.screen.height / 2 - 50;
